d8071bafb50f8cf0a3c677e2df60e65405de5221
Luis BC <luinux81@gmail.com>
2022-06-26 17:54:09 +0200

    Cambios de tipo en varios campos

--- src/admin/sql/install.mysql.sql
+++ src/admin/sql/install.mysql.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS `#__base` (
     `title`         varchar(64)     NOT NULL,
     `description`   text            NOT NULL,
     `field_1`       decimal(10,0)   NOT NULL,
-    `field_2`       tinyint(1)      NOT NULL DEFAULT '0',
-    `field_3`       int(11)         NOT NULL DEFAULT '0',
+    `field_2`       int(11)         NOT NULL DEFAULT '0',
+    `field_3`       varchar(512)    ,
     `state`         TINYINT         NOT NULL DEFAULT '1',
     PRIMARY KEY (`id`)
 
@@ -26,4 +26,4 @@ INSERT IGNORE INTO `#__base` (`id`, `title`, `description`, `field_1`, `field_2`
 (2, 'Item 2', 'El segundo item', '6', 0, 0, 1),
 (3, 'Item 3', 'El tercer item', '123', 1, 16, 0),
 (4, 'Item 4', 'El cuarto item', '4', 0, 87, 0),
-(5, 'Item 5', 'El quito item', '5', 1, 23, 1);
+(5, 'Item 5', 'El quinto item', '5', 1, 23, 1);
