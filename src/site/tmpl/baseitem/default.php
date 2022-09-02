28c4ea18cbd7d9c60489c6d6a93c084dfdf11356
Luis BC <luinux81@gmail.com>
2022-09-02 13:30:07 +0200

    Añadir contenido a vista baseitem individual

--- src/site/tmpl/baseitem/default.php
+++ src/site/tmpl/baseitem/default.php
@@ -19,7 +19,36 @@ use Joomla\CMS\Language\Text;
 
 <p><?php echo $this->item->description; ?>!</p>
 
-<h2><?php echo Text::_('COM_MYWALKS_WALK_REPORTS'); ?></h2>
+<h2><?php echo Text::_('COM_BASECOMPONENT_BASEITEM_INFO'); ?></h2>
+
+<div class="table-resposive">
+	<table class="table table-stripped">
+		<caption><?php echo Text::_('COM_BASECOMPONENT_BASEITEM_INFO'); ?></caption>
+		<thead>
+			<tr>
+				<th scope="col" style="min-width:6rem;">
+					<?php echo Text::_('COM_BASECOMPONENT_BASEITEM_FIELD1'); ?>
+				</th>
+				<th scope="col" style="min-width:6rem;">
+					<?php echo Text::_('COM_BASECOMPONENT_BASEITEM_FIELD2'); ?>
+				</th>
+				<th scope="col" style="min-width:6rem;">
+					<?php echo Text::_('COM_BASECOMPONENT_BASEITEM_FIELD3'); ?>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><?php echo $this->item->field_1; ?></td>
+				<td><?php echo $this->item->field_2; ?></td>
+				<td><?php echo $this->item->field_3; ?></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+
+<!-- <h2><?php echo Text::_('COM_MYWALKS_WALK_REPORTS'); ?></h2>
 
 <div class="table-responsive">
   <table class="table table-striped">
@@ -39,4 +68,4 @@ use Joomla\CMS\Language\Text;
 	<?php //endforeach; ?><?php //endif; ?>
 	</tbody>
   </table>
-</div>
+</div> -->
